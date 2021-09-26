447a8177305aad6a52b1d17226ad850627d03f29
marthaleyton <mleyton7@gmail.com>
2021-09-26 16:36:20 -0500

    V.1.0.0 Creacion inicial del Proyecto

---
+++ Ganaderia/Ganaderia.App/Ganaderia.App.Consola/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Ganaderia.App.Dominio;
+using Ganaderia.App.Persistencia;
+
+namespace Ganaderia.App.Consola
+{
+    class Program
+    {
+        private static IRepositorioGanadero _repositorioGanadero = new RepositorioGanadero(new Persistencia.AppContext());
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            AddGanadero();
+
+        }
+
+        private static void AddGanadero()
+        {
+            var ganadero = new Ganadero
+            {
+                Nombres = "Martha",
+                Apellidos = "Leyton",
+                NumeroTelefono = "3165434131",
+                Correo = "mleyton7@gmail.com",
+                Contrasena = "123456",
+                Latitud = 4554,
+                Longitud = 5445
+            } ;
+            _repositorioGanadero.AddGanadero(ganadero);
+        }
+
+    }
+}
---
+++ Ganaderia/Ganaderia.App/Ganaderia.App.Persistencia/AppRepositorios/RepositorioGanadero.cs
@@ -0,0 +1,22 @@
+using Ganaderia.App.Dominio;
+
+namespace Ganaderia.App.Persistencia
+{
+    public class RepositorioGanadero : IRepositorioGanadero
+    {
+        private readonly AppContext _appContext;
+
+        public RepositorioGanadero(AppContext appContext)
+        {
+            _appContext = appContext;
+        }
+        void IRepositorioGanadero.AddGanadero(Ganadero ganadero)
+        {
+            _appContext.Ganaderos.Add(ganadero);
+            _appContext.SaveChanges();
+
+        }
+    
+    }
+
+}
